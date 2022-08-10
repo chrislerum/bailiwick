6b5b1290eaf7f34021f66d330e6f034a282bc59a
Chris Lerum <chris.lerum@gmail.com>
2022-08-10 09:42:04 -0400

    x

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :hindrances
   namespace :admin do
       resources :interests
       resources :hindrances
       resources :excuses
 
-      root to: "interests#index"
+      root "interests#index"
     end
+  resources :hindrances
   resources :interests
   root "main#index"
-  get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
