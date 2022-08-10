ee32aabfdef809b732649f9f903d1ba7b78eb81f
Chris Lerum <chris.lerum@gmail.com>
2022-08-09 20:03:06 -0400

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_231302) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_000101) do
   create_table "excuses", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at", null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_231302) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
