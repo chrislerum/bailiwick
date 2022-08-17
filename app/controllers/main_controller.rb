65c2210ad7f4b6aeb9cadae55a2106d17f851362
Chris Lerum <chris.lerum@gmail.com>
2022-08-17 03:15:35 -0400

    x

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,11 @@
 class MainController < ApplicationController
+
   def index
     @interests = Interest.all
     @hindrances = Hindrance.all
     @excuses = Excuse.all
+  end
+
+  def blank
   end
 end
