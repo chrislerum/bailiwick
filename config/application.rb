89a790e30137cc999f5a20a7c00c7bec87672ab2
Chris Lerum <chris.lerum@gmail.com>
2022-09-08 13:11:43 -0400

    x

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Bailiwick
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.secret_key_base = "sfdhfsuuuhvsodvdivusyrigrskubfksdjdvbskdbskd"
 
     # Configuration for the application, engines, and railties goes here.
     #
