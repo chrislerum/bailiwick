603202c96f5c8293432a49110afecad2d134d8c0
Chris Lerum <chris.lerum@gmail.com>
2022-09-09 15:18:28 -0400

    x

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "139.144.17.207", user: "deploy", roles: %w{db}
+server "139.144.27.171", user: "deploy", roles: %w{db}
 set :application, 'bailiwick'
 set :repo_url, "git@github.com:chrislerum/bailiwick.git"
 set :chruby_ruby, '3.1.1'
